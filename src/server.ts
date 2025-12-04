5a80ed363c11fb61fa8ef4102bac30905edcf6ea
hritik2002 <sharmahritik2002@gmail.com>
2025-12-04 23:02:00 +0530

    fix: resolve build issues

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
-import app from "./app.ts";
-import { CONFIG } from "./config/index.ts";
+import dotenv from "dotenv";
+dotenv.config();
+
+import app from "./app.js";
+import { CONFIG } from "./config/index.js";
 
 app.listen(CONFIG.port, () => {
   console.log(`Server is running on port ${CONFIG.port}`);
